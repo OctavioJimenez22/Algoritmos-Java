b299d64e572f5148dbb15a6488a6db2c527546a4
Octavio Jimenez <djtavojimenez@gmail.com>
2024-04-29 00:02:21 -0500

    Agrego el algoritmo ejercicio5

---
+++ java/ejercicio5.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+public class ejercicio5 {
+
+    public  static void main(){
+
+        for(int i = 0; i<=3; i++){
+
+            System.out.println(i);
+
+        }
+
+    }
+}
